555da4e43e2e0214d213974846e5d2482ea5b114
Alina Sheyanova <sheyanova@proitr.ru>
2023-04-14 18:05:14 +0300

    tests for LionClass

--- src/test/java/LionManeParametrizedTest.java
+++ src/test/java/LionManeParametrizedTest.java
@@ -1,2 +1,36 @@
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import static org.junit.Assert.assertEquals;
+
+
+@RunWith(Parameterized.class)
 public class LionManeParametrizedTest {
+    private String sex;
+    private boolean expectedHasMane;
+    Feline feline = new Feline();
+
+    public LionManeParametrizedTest(String sex, boolean expectedHasMane) {
+        this.sex = sex;
+        this.expectedHasMane = expectedHasMane;
+    }
+
+    @Parameterized.Parameters
+    public static Object[][] shouldHaveMane() {
+        return new Object[][]{
+                {"Самец", Boolean.TRUE},
+                {"Самка", Boolean.FALSE},
+
+        };
+    }
+
+    @Test
+    public void testLionWhatIsMane() throws Exception{
+        Lion lion = new Lion(sex, feline);
+        assertEquals("Пол не совпал",expectedHasMane,lion.doesHaveMane());
+    }
+
+
 }
--- src/test/java/LionMockClassTest.java
+++ src/test/java/LionMockClassTest.java
@@ -1,2 +1,25 @@
+import com.example.Feline;
+import com.example.Lion;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(MockitoJUnitRunner.class)
 public class LionMockClassTest {
+    @Mock
+    Feline feline;
+
+    //проверка количество возвращаемых котят
+    @Test
+    public void testLionReturnsCorrectCountOfKittens() throws Exception {
+        Lion lion = new Lion("Самец", feline);
+        Mockito.when(feline.getKittens()).thenReturn(1);
+        assertEquals(1, lion.getKittens());
+    }
+
+
 }
