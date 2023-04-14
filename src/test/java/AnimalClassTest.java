ff04d6d1e444aed9c9a5be51e7e33110d3088a94
Alina Sheyanova <sheyanova@proitr.ru>
2023-04-14 15:54:30 +0300

    tests for a animalСlass have been created

--- src/test/java/AnimalClassTest.java
+++ src/test/java/AnimalClassTest.java
@@ -1,2 +1,23 @@
+import com.example.Animal;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class AnimalClassTest {
+    Animal animal = new Animal();
+
+    //проверка вывода семейств
+    @Test
+    public void testAnimalGetFoodReturnCorrectString() {
+        assertEquals("Существует несколько семейств: заячьи, беличьи, мышиные, кошачьи, псовые, медвежьи, куньи", animal.getFamily());
+    }
+
+    //проверка вызова исключения в случае несовпадения вида
+    @Test
+    public void testAnimalGetFoodReturnCorrectTrowString() {
+        String expectedAnswer = "Неизвестный вид животного, используйте значение Травоядное или Хищник";
+        Exception actualAnswer = Assert.assertThrows(Exception.class, () -> animal.getFood("Пончикоядное"));
+        assertEquals("Неправильный ответ", expectedAnswer, actualAnswer.getMessage());
+    }
 }
